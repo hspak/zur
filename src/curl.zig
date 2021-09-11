657ab38189e5b35dc5fefac71be8889d11d08106
Hong Shick Pak <hong@hspak.com>
2021-09-11 16:20:33 -0700

    chore: update for zig master

--- src/curl.zig
+++ src/curl.zig
@@ -1,31 +1,30 @@
 const std = @import("std");
 const fs = std.fs;
 
-// setting to prevent curl.curl_* stutter
-usingnamespace @cImport(@cInclude("curl/curl.h"));
+const curl = @cImport(@cInclude("curl/curl.h"));
 
 pub fn init() !void {
     // global curl init, or fail
-    if (curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
+    if (curl.curl_global_init(curl.CURL_GLOBAL_ALL) != curl.CURLE_OK) {
         return error.CURLGlobalInitFailed;
     }
 }
 
 pub fn deinit() void {
-    curl_global_cleanup();
+    curl.curl_global_cleanup();
 }
 
 pub fn get(allocator: *std.mem.Allocator, uri: [*:0]const u8) !std.ArrayList(u8) {
-    const handle = curl_easy_init() orelse return error.CURLHandleInitFailed;
-    defer curl_easy_cleanup(handle);
+    const handle = curl.curl_easy_init() orelse return error.CURLHandleInitFailed;
+    defer curl.curl_easy_cleanup(handle);
 
     var resp_buffer = std.ArrayList(u8).init(allocator);
 
-    try wrap(curl_easy_setopt(handle, CURLOPT_URL, uri));
-    try wrap(curl_easy_setopt(handle, CURLOPT_ACCEPT_ENCODING, ""));
-    try wrap(curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writeRespCallback));
-    try wrap(curl_easy_setopt(handle, CURLOPT_WRITEDATA, &resp_buffer));
-    try wrap(curl_easy_perform(handle));
+    try wrap(curl.curl_easy_setopt(handle, curl.CURLOPT_URL, uri));
+    try wrap(curl.curl_easy_setopt(handle, curl.CURLOPT_ACCEPT_ENCODING, ""));
+    try wrap(curl.curl_easy_setopt(handle, curl.CURLOPT_WRITEFUNCTION, writeRespCallback));
+    try wrap(curl.curl_easy_setopt(handle, curl.CURLOPT_WRITEDATA, &resp_buffer));
+    try wrap(curl.curl_easy_perform(handle));
 
     return resp_buffer;
 }
@@ -43,7 +42,7 @@ fn writeRespCallback(data: *c_void, size: c_uint, count: c_uint, user_data: *c_v
 
 fn wrap(result: anytype) !void {
     switch (result) {
-        CURLE_OK => return,
+        curl.CURLE_OK => return,
         else => {
             std.debug.print("curl error: {d}\n", .{result});
             // TODO: some real error handling
