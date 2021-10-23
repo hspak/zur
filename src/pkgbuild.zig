3f20580c9e20dac0af11f99262e400f6aaf08a0a
Hong Shick Pak <hong@hspak.com>
2021-10-22 17:49:20 -0700

    fix: workaround bad assumption in pkgbuild parsing

--- src/pkgbuild.zig
+++ src/pkgbuild.zig
@@ -66,19 +66,27 @@ pub const Pkgbuild = struct {
                 // PKGBUILD comments
                 '#' => {
                     while (true) {
-                        const lookahead = try stream.readByte();
+                        // footer comments cause this to return EndOfStream error
+                        const lookahead = stream.readByte() catch |err| switch (err) {
+                            error.EndOfStream => break,
+                        };
                         if (lookahead == '\n') break;
                     }
                 },
                 // PKGBUILD key=value
                 '=' => {
                     var key = buf.toOwnedSlice();
+                    var in_quotes = false;
                     while (true) {
                         const lookahead = try stream.readByte();
                         if (lookahead == '(') {
                             while (true) {
                                 const moreahead = try stream.readByte();
-                                if (moreahead == ')') break;
+
+                                // This naive parsing goofs when there are parens in quotes
+                                if (moreahead == '\'') in_quotes = !in_quotes;
+
+                                if (moreahead == ')' and !in_quotes) break;
                                 if (moreahead != ' ' and moreahead != '\t') {
                                     try buf.append(moreahead);
                                 }
@@ -174,6 +182,114 @@ pub const Pkgbuild = struct {
         }
     }
 };
+
+test "Pkgbuild - readLines - neovim-git" {
+    var file_contents =
+        \\# Maintainer: Florian Walch <florian+aur@fwalch.com>
+        \\# Contributor: Florian Hahn <flo@fhahn.com>
+        \\# Contributor: Sven-Hendrik Haase <svenstaro@gmail.com>
+        \\
+        \\pkgname=neovim-git
+        \\pkgver=0.4.0.r2972.g3fbff98cf
+        \\pkgrel=1
+        \\pkgdesc='Fork of Vim aiming to improve user experience, plugins, and GUIs.'
+        \\arch=('i686' 'x86_64' 'armv7h' 'armv6h' 'aarch64')
+        \\url='https://neovim.io'
+        \\backup=('etc/xdg/nvim/sysinit.vim')
+        \\license=('custom:neovim')
+        \\depends=('libluv' 'libtermkey' 'libutf8proc' 'libuv' 'libvterm>=0.1.git5' 'luajit' 'msgpack-c' 'unibilium' 'tree-sitter')
+        \\makedepends=('cmake' 'git' 'gperf' 'lua51-mpack' 'lua51-lpeg')
+        \\optdepends=('python2-neovim: for Python 2 plugin support (see :help provider-python)'
+        \\            'python-neovim: for Python 3 plugin support (see :help provider-python)'
+        \\            'ruby-neovim: for Ruby plugin support (see :help provider-ruby)'
+        \\            'xclip: for clipboard support (or xsel) (see :help provider-clipboard)'
+        \\            'xsel: for clipboard support (or xclip) (see :help provider-clipboard)'
+        \\            'wl-clipboard: for clipboard support on wayland (see :help clipboard)')
+        \\source=("${pkgname}::git+https://github.com/neovim/neovim.git")
+        \\sha256sums=('SKIP')
+        \\provides=("neovim=${pkgver}" 'vim-plugin-runtime')
+        \\conflicts=('neovim')
+        \\install=neovim-git.install
+        \\options=(!strip)
+        \\
+        \\pkgver() {
+        \\  cd "${pkgname}"
+        \\  git describe --long | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g'
+        \\}
+        \\
+        \\build() {
+        \\  cmake -S"${pkgname}" -Bbuild \
+        \\        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        \\        -DCMAKE_INSTALL_PREFIX=/usr
+        \\  cmake --build build
+        \\}
+        \\
+        \\check() {
+        \\  cd "${srcdir}/build"
+        \\  ./bin/nvim --version
+        \\  ./bin/nvim --headless -u NONE -i NONE -c ':quit'
+        \\}
+        \\
+        \\package() {
+        \\  cd "${srcdir}/build"
+        \\  DESTDIR="${pkgdir}" cmake --build . --target install
+        \\
+        \\  cd "${srcdir}/${pkgname}"
+        \\  install -Dm644 LICENSE "${pkgdir}/usr/share/licenses/${pkgname}/LICENSE"
+        \\  install -Dm644 runtime/nvim.desktop "${pkgdir}/usr/share/applications/nvim.desktop"
+        \\  install -Dm644 runtime/nvim.png "${pkgdir}/usr/share/pixmaps/nvim.png"
+        \\
+        \\  # Make Arch vim packages work
+        \\  mkdir -p "${pkgdir}"/etc/xdg/nvim
+        \\  echo "\" This line makes pacman-installed global Arch Linux vim packages work." > "${pkgdir}"/etc/xdg/nvim/sysinit.vim
+        \\  echo "source /usr/share/nvim/archlinux.vim" >> "${pkgdir}"/etc/xdg/nvim/sysinit.vim
+        \\
+        \\  mkdir -p "${pkgdir}"/usr/share/vim
+        \\  echo "set runtimepath+=/usr/share/vim/vimfiles" > "${pkgdir}"/usr/share/nvim/archlinux.vim
+        \\}
+        \\
+        \\# vim:set sw=2 sts=2 et:
+    ;
+    var expectedMap = std.StringHashMap(*Content).init(testing.allocator);
+    defer expectedMap.deinit();
+
+    var install_val = std.ArrayList(u8).init(testing.allocator);
+    try install_val.appendSlice("neovim-git.install");
+    var install_content = try Content.init(testing.allocator, install_val.toOwnedSlice());
+    defer install_content.deinit(testing.allocator);
+    try expectedMap.putNoClobber("install", install_content);
+
+    var package_val = std.ArrayList(u8).init(testing.allocator);
+    try package_val.appendSlice(
+        \\{
+        \\  cd "${srcdir}/build"
+        \\  DESTDIR="${pkgdir}" cmake --build . --target install
+        \\
+        \\  cd "${srcdir}/${pkgname}"
+        \\  install -Dm644 LICENSE "${pkgdir}/usr/share/licenses/${pkgname}/LICENSE"
+        \\  install -Dm644 runtime/nvim.desktop "${pkgdir}/usr/share/applications/nvim.desktop"
+        \\  install -Dm644 runtime/nvim.png "${pkgdir}/usr/share/pixmaps/nvim.png"
+        \\
+        \\  # Make Arch vim packages work
+        \\  mkdir -p "${pkgdir}"/etc/xdg/nvim
+        \\  echo "\" This line makes pacman-installed global Arch Linux vim packages work." > "${pkgdir}"/etc/xdg/nvim/sysinit.vim
+        \\  echo "source /usr/share/nvim/archlinux.vim" >> "${pkgdir}"/etc/xdg/nvim/sysinit.vim
+        \\
+        \\  mkdir -p "${pkgdir}"/usr/share/vim
+        \\  echo "set runtimepath+=/usr/share/vim/vimfiles" > "${pkgdir}"/usr/share/nvim/archlinux.vim
+        \\}
+    );
+    var package_content = try Content.init(testing.allocator, package_val.toOwnedSlice());
+    defer package_content.deinit(testing.allocator);
+    try expectedMap.putNoClobber("package()", package_content);
+
+    var pkgbuild = Pkgbuild.init(testing.allocator, file_contents);
+    defer pkgbuild.deinit();
+    try pkgbuild.readLines();
+
+    try testing.expectEqualStrings(expectedMap.get("install").?.value, pkgbuild.fields.get("install").?.value);
+    try testing.expectEqualStrings(expectedMap.get("package()").?.value, pkgbuild.fields.get("package()").?.value);
+}
 
 test "Pkgbuild - readLines - google-chrome-dev" {
     var file_contents =
