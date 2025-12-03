018f16cd0d931cb3267707c80c17fe124533ea41
Hong Shick Pak <hong@hspak.com>
2025-12-02 21:00:46 -0800

    fix: use proper replacement for allocPrintZ

--- src/alpm.zig
+++ src/alpm.zig
@@ -4,8 +4,8 @@ const alpm = @cImport({
 });
 
 pub fn is_newer_than(allocator: std.mem.Allocator, ver_a: []const u8, ver_b: []const u8) !bool {
-    const ver_a_cstr: [*c]u8 = @ptrCast(try std.fmt.allocPrintSentinel(allocator, "{s}", .{ver_a}, '0'));
-    const ver_b_cstr: [*c]u8 = @ptrCast(try std.fmt.allocPrintSentinel(allocator, "{s}", .{ver_b}, '0'));
+    const ver_a_cstr: [*c]u8 = @ptrCast(try std.mem.concatWithSentinel(allocator, u8, &.{ ver_a }, 0));
+    const ver_b_cstr: [*c]u8 = @ptrCast(try std.mem.concatWithSentinel(allocator, u8, &.{ ver_b }, 0));
 
     const ret = alpm.alpm_pkg_vercmp(ver_a_cstr, ver_b_cstr);
     if (ret == 1) {
